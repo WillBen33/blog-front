40f716a0e6216f41bfef1983b4eb2906b64b6560
Kaylinhh <ly.julie@live.fr>
2023-08-02 14:30:29 +0200

    initial commit

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CreateArticleComponent } from './components/feature/create-article/create-article.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateArticleComponent
+  ],
   imports: [
     CommonModule
   ]
